f0c09b6a7a58f012a10218a4f4d98661b0daa650
WarrenWoo <wuwenlong@robkoo.com>
2022-02-08 16:01:30 +0800

    添加 view 内嵌 view 旋转

--- 5.3/Transform3D/Transform3D/ViewController.swift
+++ 5.3/Transform3D/Transform3D/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var layerView2: UIView!
     
+    @IBOutlet weak var rotateOuterView: UIView!
+    
+    @IBOutlet weak var rotateInnerView: UIView!
+    
+    @IBOutlet weak var rotateYOuterView: UIView!
+    
+    @IBOutlet weak var rotateYInnerView: UIView!
+    
+    
     let rocketImageName = "rocket"
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,10 +39,15 @@ class ViewController: UIViewController {
         self.layerView.layer.contents = UIImage(named: rocketImageName)?.cgImage
         self.layerView.layer.contentsGravity = .resizeAspect
         self.layerView.layer.contentsScale = UIScreen.main.scale
+        // 是否双面 绘制
+        self.layerView.layer.isDoubleSided = false
         
         self.layerView2.layer.contents = UIImage(named: rocketImageName)?.cgImage
         self.layerView2.layer.contentsGravity = .resizeAspect
         self.layerView2.layer.contentsScale = UIScreen.main.scale
+        // 是否双面 绘制
+        self.layerView2.layer.isDoubleSided = false
+        
         // 默认的 3D 旋转矩阵
         var transform = CATransform3DIdentity
         var transform2 = CATransform3DIdentity
@@ -45,15 +59,34 @@ class ViewController: UIViewController {
         
         // 延 y 轴旋转 π/4
         transform = CATransform3DRotate(transform, Double.pi/4, 0, 1, 0)
-        transform2 = CATransform3DRotate(transform2, -Double.pi/4, 0, 1, 0)
+        transform2 = CATransform3DRotate(transform2, -(Double.pi/4), 0, 1, 0)
         
         
         self.layerView.layer.transform = transform
         self.layerView2.layer.transform = transform2
         
+    
+        /** 绕着 z 轴旋转 **/
+        let outrotate = CATransform3DMakeRotation(Double.pi / 4, 0, 0, 1)
+        self.rotateOuterView.layer.transform = outrotate
+        
+        let innerRotate = CATransform3DMakeRotation(-(Double.pi / 4), 0, 0, 1)
+        self.rotateInnerView.layer.transform = innerRotate
         
         
+        /** 绕着 y 轴旋转 双层旋转*/
         
+        // 外层视图绕着 Y 轴旋转45度
+        var rotateYout = CATransform3DIdentity
+//        rotateYout.m34 = -1.0 / 500.0
+        rotateYout = CATransform3DRotate(rotateYout, Double.pi / 4, 0, 1, 0)
+        self.rotateYOuterView.layer.transform = rotateYout
+        
+        // 内层视图 绕着 Y 轴旋转 - 45度
+        var rotateYinner = CATransform3DIdentity
+        rotateYinner.m34 = -1.0 / 500.0
+        rotateYinner = CATransform3DRotate(rotateYinner, -(Double.pi / 4), 0, 1, 0)
+        self.rotateYInnerView.layer.transform = rotateYinner
     }
 
 
